2f0ca3e4ec1da2c73ed4983f6a16c61167f00ec2
Tarjei Husøy <tarjei@roms.no>
2014-01-13 22:53:31 +0100

    Make filetypes to include configurable

--- husoy-lipsanen.py
+++ husoy-lipsanen.py
@@ -6,18 +6,22 @@ import os.path
 import matplotlib.pyplot as plt
 import numpy as np
 
-FILETYPES = 'gif jpeg jpg ppt png doc xls xlsx svg txt csv pdf tex'.split()
+_DEFAULT_FILETYPES = 'gif,jpeg,jpg,ppt,png,doc,xls,xlsx,svg,txt,csv,pdf,tex'
 
 def main():
     args = get_arguments()
     frequencies = get_frequencies(args.personal_directory)
-    histogram(frequencies)
+    histogram(frequencies, args.types)
 
 
 def get_arguments():
     argparser = argparse.ArgumentParser('husoy-lipsanen')
     argparser.add_argument('personal_directory', help='The directory to run the profiling on')
-    return argparser.parse_args()
+    argparser.add_argument('-t', '--types', default=_DEFAULT_FILETYPES,
+        help='Comma-separated list of extensions to use for the graphing')
+    args = argparser.parse_args()
+    args.types = args.types.split(',')
+    return args
 
 
 def get_frequencies(directory):
@@ -28,11 +32,11 @@ def get_frequencies(directory):
     return counter
 
 
-def histogram(frequencies):
-    elements = [frequencies[t] for t in FILETYPES]
+def histogram(frequencies, filetypes):
+    elements = [frequencies[t] for t in filetypes]
     y_pos = np.arange(len(elements))
     plt.barh(y_pos, elements, align='center')
-    plt.yticks(y_pos, FILETYPES)
+    plt.yticks(y_pos, filetypes)
     plt.show()
 
 if __name__ == '__main__':
