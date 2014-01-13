4b3950bc396d2758a8838ccc921686c552905f9a
Tarjei Husøy <tarjei@roms.no>
2014-01-13 23:20:45 +0100

    Add option to exclude certain directories

--- husoy-lipsanen.py
+++ husoy-lipsanen.py
@@ -10,7 +10,7 @@ _DEFAULT_FILETYPES = 'gif,jpeg,jpg,ppt,png,doc,xls,xlsx,svg,txt,csv,pdf,tex'
 
 def main():
     args = get_arguments()
-    frequencies = get_frequencies(args.personal_directory)
+    frequencies = get_frequencies(args.personal_directory, args.ignore)
     histogram(frequencies, args.types)
 
 
@@ -18,16 +18,20 @@ def get_arguments():
     argparser = argparse.ArgumentParser('husoy-lipsanen')
     argparser.add_argument('personal_directory', help='The directory to run the profiling on')
     argparser.add_argument('-t', '--types', default=_DEFAULT_FILETYPES,
-        help='Comma-separated list of extensions to use for the graphing')
+        help='Comma-separated list of extensions to use for the graphing. Default: %(default)s')
+    argparser.add_argument('-i', '--ignore', help='Directory names to ignore. Default: %(default)s')
     args = argparser.parse_args()
     args.types = args.types.split(',')
+    args.ignore = set(args.ignore.split(','))
     return args
 
 
-def get_frequencies(directory):
+def get_frequencies(directory, ignore_dirs):
     counter = Counter()
     for root, dirs, files in os.walk(directory):
-        print('Couting files in %s...' % root)
+        dir_set = set(dirs) - ignore_dirs
+        dirs[:] = list(dir_set)
+        print('Analyzing files in %s...' % root)
         counter.update(os.path.splitext(file)[1][1:] for file in files)
     return counter
 
